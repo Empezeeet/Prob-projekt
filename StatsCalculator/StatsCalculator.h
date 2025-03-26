038092544ab74ec37d88a2b9a6f876455c62af27
Empezeeet <pawelkomateusz@gmail.com>
2025-03-26 21:59:46 +0000

    Added Stats Calculation Code

--- StatsCalculator/StatsCalculator.h
+++ StatsCalculator/StatsCalculator.h
@@ -1,20 +1,23 @@
 #ifndef STATS_CALCULATOR_H
 #define STATS_CALCULATOR_H
 
-
-
+#include "../Manager/Manager.h"
+#include <ctime>
 namespace Program {
+    
     class StatsCalculator {
 
-        public:
+        private:
         /*Available stats:
             - usage Liters per 100 km
             - monthly expenses
             - all-time expenses
             - 
         */
-
-
+        static float calculateAllTimeExpenses(LogArray* logs);
+        static float calculateMonthlyExpenses(LogArray* logs);
+        static float fuelUsage(LogArray* logs, Program::Auto* car);
+        
 
 
     };
